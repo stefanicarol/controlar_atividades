92e0834c01543d223727d3f5b4aa31a5fd3b9c1c
Stéfani Carol Souza <stefanicarol@gmail.com>
2020-05-30 20:06:36 -0300

    setimo commit

--- src/controleatividades/Forms/FormMenu.java
+++ src/controleatividades/Forms/FormMenu.java
@@ -92,6 +92,7 @@ public class FormMenu extends javax.swing.JFrame {
         jMenuItem5 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
+        jMenuItem8 = new javax.swing.JMenuItem();
 
         jMenuItem1.setText("jMenuItem1");
 
@@ -147,6 +148,15 @@ public class FormMenu extends javax.swing.JFrame {
         jMenuBar1.add(jMenu2);
 
         jMenu3.setText("SOBRE");
+
+        jMenuItem8.setText("DESENVOLVEDORES");
+        jMenuItem8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem8ActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jMenuItem8);
+
         jMenuBar1.add(jMenu3);
 
         setJMenuBar(jMenuBar1);
@@ -190,6 +200,11 @@ public class FormMenu extends javax.swing.JFrame {
         FormAtividade formatividade = new FormAtividade(conteudo, questao, turma, alternativa, atividade);
         formatividade.setVisible(true);
     }//GEN-LAST:event_jMenuItem5ActionPerformed
+
+    private void jMenuItem8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem8ActionPerformed
+         FormSobre forms = new FormSobre();
+        forms.setVisible(true);
+    }//GEN-LAST:event_jMenuItem8ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -238,5 +253,6 @@ public class FormMenu extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
     private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
     // End of variables declaration//GEN-END:variables
 }
